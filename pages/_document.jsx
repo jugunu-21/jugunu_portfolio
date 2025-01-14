22f9b7340c24157dcc13fef8427c06c33270e24f
jugunu-21 <aaruhijugunu@gmail.com>
2025-01-15 01:01:27 +0530

    add hotjar analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,6 +13,19 @@ class MyDocument extends Document {
       <Html lang="en" className='scroll-smooth'>
         <Head>
           <Script
+            strategy="afterInteractive" // Ensures the script loads after the page is interactive
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+                h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+                h._hjSettings={hjid:5267430,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+            }}
+          />
+          <Script
             strategy="afterInteractive"
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
